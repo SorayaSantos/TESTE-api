0ea7b27ea694d12b5c6cf5ee6302dbb5316f4eec
SorayaSantos <soraya.ss_3@hotmail.com>
2018-12-03 19:37:16 +0000

    fim da aula 3 dez

--- sexta-feira/src/main/java/io/altar/sextafeira/EndPoint.java
+++ sexta-feira/src/main/java/io/altar/sextafeira/EndPoint.java
@@ -1,6 +1,11 @@
 package io.altar.sextafeira;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,5 +32,22 @@ public class EndPoint {
 	    return "hello " + name;
 	}
 	
+	private static Map<Long, Car> carMap = new HashMap<Long,Car>();
+	static Long id=(long) 1;
 	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Car createCar(Car car){
+		carMap.put(id, car);
+		id++;
+		return car;
+	}
+	@GET
+	@Path("car/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Car seeCar(@PathParam("id") Long id) {
+		return carMap.get(id);
+	}
+ 	
 }
